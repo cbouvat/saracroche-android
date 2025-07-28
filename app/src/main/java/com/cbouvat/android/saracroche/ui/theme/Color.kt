134d4ee2f8ace1f8b84b7d89322c0ac6d9c4b909
Camille Bouvat <mail@cbouvat.com>
2025-07-28 14:52:14 +0200

    Update color scheme to use Saracroche brand colors

--- app/src/main/java/com/cbouvat/android/saracroche/ui/theme/Color.kt
+++ app/src/main/java/com/cbouvat/android/saracroche/ui/theme/Color.kt
@@ -3,6 +3,5 @@ package com.cbouvat.android.saracroche.ui.theme
 import androidx.compose.ui.graphics.Color
 
 // Saracroche brand colors
-val SaracrocheBlue = Color(0xFF2196F3)
-val SarcrocheTeal = Color(0xFF009688)
-val SaracrocheOrange = Color(0xFFFF5722)
+val SaracrochePrimary = Color(0xFFFFC600)
+val SarcrocheSecondary = Color(0xFFD9D9D9)
--- app/src/main/java/com/cbouvat/android/saracroche/ui/theme/Theme.kt
+++ app/src/main/java/com/cbouvat/android/saracroche/ui/theme/Theme.kt
@@ -7,25 +7,13 @@ import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 
 private val DarkColorScheme = darkColorScheme(
-    primary = SaracrocheBlue,
-    secondary = SarcrocheTeal,
-    tertiary = SaracrocheOrange
+    primary = SaracrochePrimary,
+    secondary = SarcrocheSecondary,
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = SaracrocheBlue,
-    secondary = SarcrocheTeal,
-    tertiary = SaracrocheOrange,
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    primary = SaracrochePrimary,
+    secondary = SarcrocheSecondary,
 )
 
 @Composable
