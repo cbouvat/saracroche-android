3f96f63a02534e5c989bce5b5fe3d2ef749bb7ac
Camille Bouvat <mail@cbouvat.com>
2025-11-27 10:47:14 +0100

    Bump versionCode to 20 and versionName to 2.4.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,9 +12,9 @@ android {
         applicationId = "com.cbouvat.android.saracroche"
         minSdk = 29
         targetSdk = 36
-        versionCode = 19
+        versionCode = 20
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        versionName = "2.3.0"
+        versionName = "2.4.0"
     }
 
     buildTypes {
